d828e87a563e871f74bf550de91e9aa3b14d2a24
Nabhan Maswood <nmaswood98@gmail.com>
2019-10-15 01:36:44 -0400

    Starting image viewer

---
+++ tent_ios/tent_ios/TentView.swift
@@ -0,0 +1,36 @@
+//
+//  TentView.swift
+//  tent_ios
+//
+//  where you view the files in a tent
+//  Created by Nabhan Maswood on 10/15/19.
+//  Copyright © 2019 Nabhan Maswood. All rights reserved.
+//
+
+import SwiftUI
+
+struct TentView: View {
+    let tent = Tent()
+    var body: some View {
+        List {
+        
+            ForEach(tent.rows) { row in
+                HStack(alignment: .center) {
+                    ForEach(row.cells) { cell in
+                        Image(cell.imageURL)
+                            .resizable()
+                            .scaledToFit()
+                        
+                    }
+                }
+            }
+        
+        }
+    }
+}
+
+struct TentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TentView()
+    }
+}
