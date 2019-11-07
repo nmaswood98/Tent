d213c427699c42ffc9c898cbff9d6294097bb155
Nabhan Maswood <nmaswood98@gmail.com>
2019-11-07 00:36:32 -0500

    Modified google cloud funciton JoinTent to be callable from in the app

--- cloud_functions/JoinTent/index.js
+++ cloud_functions/JoinTent/index.js
@@ -1,7 +1,3 @@
-// Nabhan Maswood
-// This cloud function is for when a user wants to join a tent. 
-// It will verify the tent exists and if the user can join the tent
-
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
@@ -12,12 +8,11 @@ admin.initializeApp({
 let name = "NOPE";
 const db = admin.firestore()
 
-//req.body.code is a string
-// returns the code if sucessfully joined
-// returns "False" if you did not join
-exports.enterTent = async (req, res) => {
-
-    let code = req.body.code;
+exports.enterTent = functions.https.onCall((data, context) => {
+  return new Promise(async (resolve, reject) => {
+    
+  
+    let code = data.code;
     let enteredTent = false;
     let tentLoginRef = db.collection('TentLogins').doc(code);
     await tentLoginRef.get().then(snapshot =>{
@@ -29,6 +24,8 @@ exports.enterTent = async (req, res) => {
       }
     });
   
-	res.status(200).send(enteredTent ? code : "False");
+	
+    resolve(enteredTent ? code : "False")
+    });
 
-};
+});
