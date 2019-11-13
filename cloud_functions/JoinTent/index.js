98542e7580bbaa36be6d3f749fdd502d344fa021
Nabhan Maswood <nmaswood98@gmail.com>
2019-11-12 23:43:49 -0500

    Changed google cloud function to verify location before allowing you to join the tent

--- cloud_functions/JoinTent/index.js
+++ cloud_functions/JoinTent/index.js
@@ -8,23 +8,49 @@ admin.initializeApp({
 let name = "NOPE";
 const db = admin.firestore()
 
+function isUserInTent(userLoc, tent){
+    
+  let deltaLat = tent.lat - userLoc.lat;
+  let deltaLong = tent.long - userLoc.long;
+
+  let a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
+      Math.cos(userLoc.lat) * Math.cos(tent.lat) *
+      Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
+
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+  let tentDist = (6371 * c);
+
+  return (tentDist <= tent.radius);
+
+}
+
 exports.enterTent = functions.https.onCall((data, context) => {
   return new Promise(async (resolve, reject) => {
-    
-  
+
     let code = data.code;
-    let tentName = "False";
+    let tentName = "";
+    let foundTent = false;
+    
     let tentLoginRef = db.collection('TentLogins').doc(code).collection('Tents');
     await tentLoginRef.get().then(snapshot =>{
-        	snapshot.forEach(doc => {
-             	let docName = doc.data().name;
-              	if(docName){
-                	tentName = docName;
+          snapshot.forEach(doc => {
+              if (!foundTent) {
+                let tentData = doc.data();
+                if (isUserInTent(data,tentData)){
+                  tentName = tentData.name;
+                  foundTent = true;
                 }
-   	 		});
+              }
+          });
     });
-  
-    resolve(tentName)
-    })
 
+    if(foundTent){
+      resolve(tentName);
+    }
+    else {
+      reject("Couldn't Find Tent");
+    }
+
+    });
 });
