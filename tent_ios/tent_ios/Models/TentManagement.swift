f42a1c995ebd797eb43023b25821c6476add28ca
Nabhan Maswood <nmaswood98@gmail.com>
2019-11-15 22:36:38 -0500

    Added loading indictor to tent creation and tent deletion

--- tent_ios/tent_ios/Models/TentManagement.swift
+++ tent_ios/tent_ios/Models/TentManagement.swift
@@ -19,10 +19,11 @@ class TentManagement : ObservableObject {
     init(){
     }
     
-    func createTent(location:CLLocationCoordinate2D, radius: Double, config: TentConfig,displayAlert: Binding<Bool>){
+    func createTent(location:CLLocationCoordinate2D, radius: Double, config: TentConfig,displayAlert: Binding<Bool>, loadingAlert: Binding<Bool>){
         print("Creating Tent")
     functions.httpsCallable("CreateTent").call(["lat":location.latitude.radian,"long":location.longitude.radian,"radius":radius]){ (result,error) in
             print("Got Creation result")
+            loadingAlert.wrappedValue = false
             if let error = error as NSError? {
                 print(error)
                 displayAlert.wrappedValue = true
@@ -37,12 +38,15 @@ class TentManagement : ObservableObject {
             }
             
         }
+        loadingAlert.wrappedValue = true
     }
     
-    func submitCode(value: String, location: CLLocationCoordinate2D, config: TentConfig, displayAlert: Binding<Bool>){
+    func submitCode(value: String, location: CLLocationCoordinate2D, config: TentConfig, displayAlert: Binding<Bool>, loadingAlert: Binding<Bool>){
         print("Submitting Code")
         functions.httpsCallable("JoinTent").call(["code": value,"lat":location.latitude.radian,"long":location.longitude.radian]) { (result, error) in
             print("Got code result")
+            loadingAlert.wrappedValue = false
+
           if let error = error as NSError? {
             print(error)
             displayAlert.wrappedValue = true
@@ -60,6 +64,7 @@ class TentManagement : ObservableObject {
                 
           }
         }
+        loadingAlert.wrappedValue = true
     }
     
 }
