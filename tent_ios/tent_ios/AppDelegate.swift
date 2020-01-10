ebd02d0d79d68598b7f63cc7d444350688435fc7
nmaswood98 <nmaswood98@gmail.com>
2020-01-09 19:00:57 -0500

    Changed the join tent behavior for keyboard

--- tent_ios/tent_ios/AppDelegate.swift
+++ tent_ios/tent_ios/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Firebase
 import GoogleMaps
+import Introspect
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- tent_ios/tent_ios/MainViews/TentJoinView.swift
+++ tent_ios/tent_ios/MainViews/TentJoinView.swift
@@ -9,14 +9,19 @@
 import SwiftUI
 
 struct TentJoinView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @ObservedObject var locationManager: LocationManager
     @State private var code: String = ""
     @State  var showAlert: Bool = false
     @State var showLoading: Bool = false
     @EnvironmentObject var tentConfig: TentConfig
+    @State private var radius: Double = 0
+    @State private var shouldOpenKeyboard: Bool = true
+
     var tentManagement: TentManagement = TentManagement()
     var body: some View {
         LoadingView(message: "Joining...", isShowing: $showLoading) {
+            
             
             VStack(alignment: .center) {
                 
@@ -24,23 +29,52 @@ struct TentJoinView: View {
                     Text("Current Tent: None")
                         .font(.title)
                         .foregroundColor(.green)
-                        .padding(.bottom,30)
+                        .padding(.top,30)
                 }
                 else{
                     Text("Current Tent: \(self.tentConfig.code)")
                         .font(.title)
                         .foregroundColor(.green)
-                        .padding(.bottom,30)
+                        .padding(.top,30)
                 }
                 
+                MapView(currentPosition: self.locationManager.currentLocation, circleRadius: self.radius + 3)
+                    .frame(height:300)
+                    .padding(.top, 15)
                 
-                TextField("Code", text:self.$code)
+                TextField("Code", text:self.$code,onCommit: {
+                    self.tentManagement.submitCode(value: self.code, location: self.locationManager.currentLocation, config:self.tentConfig, displayAlert:self.$showAlert, loadingAlert: self.$showLoading, completion: { status in
+                            if (status){
+                                print("Completed")
+                                self.presentationMode.wrappedValue.dismiss()
+                                UIApplication.shared.endEditing()
+                                
+                            }
+                            else{
+                                print("Completed but failed")
+                                self.shouldOpenKeyboard = true
+                                self.code = ""
+                            }
+                        })
+                    
+                })
                     .multilineTextAlignment(.center)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.allCharacters)
+                    .keyboardType(.alphabet)
+                    .padding(.top, 10)
+                    .introspectTextField { textField in
+                        if(self.shouldOpenKeyboard){
+                            textField.becomeFirstResponder()
+                            self.shouldOpenKeyboard = false
+                        }
+                    }
+                 
+ 
+
                 
-                
-                Button(action:{self.tentManagement.submitCode(value: self.code, location: self.locationManager.currentLocation, config:self.tentConfig, displayAlert:self.$showAlert, loadingAlert: self.$showLoading)}){
+                Button(action:{self.tentManagement.submitCode(value: self.code, location: self.locationManager.currentLocation, config:self.tentConfig, displayAlert:self.$showAlert, loadingAlert: self.$showLoading, completion: {status in})}){
                     Text("Enter")
                         .font(.body)
                         .foregroundColor(.green)
@@ -48,6 +82,8 @@ struct TentJoinView: View {
                 }.alert(isPresented: self.$showAlert, content: {
                     Alert(title: Text("Invaild Code"), message: Text("This Isn't a valid code"), dismissButton: .default(Text("Ok!")))
                 })
+                
+                Spacer()
                 
             }.padding(15)
             
