f46532d4bd89093c80d8a99059891747096e968b
nmaswood98 <nmaswood98@gmail.com>
2020-01-09 20:14:49 -0500

    Created CameraSnapView that flashes black when the user taps the screen

---
+++ tent_ios/tent_ios/OtherViews/CameraSnapView.swift
@@ -0,0 +1,34 @@
+//
+//  CameraSnapView.swift
+//  tent_ios
+//
+//  Created by Nabhan Maswood on 1/9/20.
+//  Copyright © 2020 Nabhan Maswood. All rights reserved.
+//
+
+import SwiftUI
+
+struct CameraSnapView: View {
+    @Binding var shouldFlash: Bool
+    var body: some View {
+        
+        ZStack{
+            
+            if(self.shouldFlash){
+                Rectangle()
+                .fill(Color.black)
+                .edgesIgnoringSafeArea(.all)
+                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: Alignment.topLeading)
+                .opacity(1)
+                .onAppear{
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.06) {
+                        self.shouldFlash  = false
+                    }
+                }
+            }
+            
+        }
+
+    }
+}
+
