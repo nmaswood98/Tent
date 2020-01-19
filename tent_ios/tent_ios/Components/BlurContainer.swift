741c30c1d13799fb6ca36eeb0b6720156be94fac
nmaswood98 <nmaswood98@gmail.com>
2020-01-19 01:39:31 -0500

    Created Blur Container

--- tent_ios/tent_ios/Components/BlurContainer.swift
+++ tent_ios/tent_ios/Components/BlurContainer.swift
@@ -10,23 +10,10 @@ import SwiftUI
 
 struct BlurContainer<Container: View>: View {
     var container: () -> Container
-    var backTap: () -> ()
+
     var body: some View {
         ZStack{
-            BlurView(style:.light)
-            VStack{
-                HStack{
-                    Button(action:self.backTap){
-                        Text("Back")
-                            .foregroundColor(.green)
-                            .font(.system(size: 20))
-                    }
-                    .padding(.top,30)
-                    .padding(.leading,20)
-                    Spacer()
-                }
-                Spacer()
-            }
+            BlurView(style:.dark)
             self.container()
         }
     }
