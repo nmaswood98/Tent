16ce515c85bea483089ccb9bfd3a1bceadcb5ad7
nmaswood98 <nmaswood98@gmail.com>
2020-01-20 23:12:56 -0500

    Renamed view folder

---
+++ UIMockUps/UIMockUps/TentHistoryView.swift
@@ -0,0 +1,21 @@
+//
+//  TentHistoryView.swift
+//  UIMockUps
+//
+//  Created by Nabhan Maswood on 1/20/20.
+//  Copyright © 2020 Nabhan Maswood. All rights reserved.
+//
+
+import SwiftUI
+
+struct TentHistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TentHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        TentHistoryView()
+    }
+}
