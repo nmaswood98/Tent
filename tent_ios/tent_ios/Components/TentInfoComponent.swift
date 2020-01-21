cc9ec07bb9b60cf2b35ea41edc4a78716463bf8e
nmaswood98 <nmaswood98@gmail.com>
2020-01-20 23:12:56 -0500

    Renamed view folder

---
+++ tent_ios/tent_ios/Components/TentInfoComponent.swift
@@ -0,0 +1,21 @@
+//
+//  TentInfoComponent.swift
+//  tent_ios
+//
+//  Created by Nabhan Maswood on 1/20/20.
+//  Copyright © 2020 Nabhan Maswood. All rights reserved.
+//
+
+import SwiftUI
+
+struct TentInfoComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TentInfoComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        TentInfoComponent()
+    }
+}
---
+++ tent_ios/tent_ios/MainPages/TentInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  TentInfoView.swift
+//  tent_ios
+//
+//  Created by Nabhan Maswood on 1/19/20.
+//  Copyright © 2020 Nabhan Maswood. All rights reserved.
+//
+
+import SwiftUI
+
+struct TentInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TentInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TentInfoView()
+    }
+}
