6348742974ab3a90ad1ba6c7349914e78b9c3f14
nmaswood98 <nmaswood98@gmail.com>
2020-01-23 00:50:51 -0500

    Created tent history save flow

--- tent_ios/tent_ios/Models/TentConfig.swift
+++ tent_ios/tent_ios/Models/TentConfig.swift
@@ -10,10 +10,14 @@ import Foundation
 import SwiftUI
 
 class TentConfig: ObservableObject{
-    @Published var name: String = "DefaultTent" {
+    @Published var name: String = "DefaultTent" { // Last property to always get set
         didSet{
             if let tContent = self.tentContent {
                 tContent.updateTent() // Update the tent when the name changes
+                
+                self.tentHistory[self.name] = TentData(code: self.code, tentLoc: self.tentLocation)
+                TentData.saveTentHistory(arr: self.tentHistory)
+                
             }
         }
     }
@@ -21,10 +25,18 @@ class TentConfig: ObservableObject{
     @Published var code: String = ""
     
     var tentLocation: TentLocation = TentLocation(lat: 0, long: 0, radius: 0)
+    var tentHistory: [String:TentData]
     
     init(){
         self.code = UserDefaults.standard.string(forKey: "tentCode") ?? ""
         self.name = UserDefaults.standard.string(forKey: "tentName") ?? "DefaultTent"
+        
+        if let tHistory = TentData.getTentHistory() {
+            self.tentHistory = tHistory
+        }
+        else{
+            self.tentHistory = [:]
+        }
     }
     
     var tentContent: TentContent? = nil
@@ -32,7 +44,9 @@ class TentConfig: ObservableObject{
     func persistData(){
         UserDefaults.standard.set(self.code, forKey: "tentCode")
         UserDefaults.standard.set(self.name, forKey: "tentName")
+        TentData.saveTentHistory(arr: self.tentHistory)
     }
+    
     
     
 }
