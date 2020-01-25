8d543fc112d31c384733d4e67e500251105c0289
nmaswood98 <nmaswood98@gmail.com>
2020-01-25 18:03:56 -0500

    Finished TentGallery Class

--- tent_ios/tent_ios/Models/TentGallery.swift
+++ tent_ios/tent_ios/Models/TentGallery.swift
@@ -12,11 +12,93 @@ import Kingfisher
 import FirebaseFirestore
 
 class TentGallery: ObservableObject{
-    let imageList: [TentImage] = []
+    var imagesToDisplay: [TentImage] = [] // All images
+    var imageList: [TentImage] = [] // All images including ones that are not downloaded yet
     
+    let db = Firestore.firestore()
+    var tentName: String
+    var tentConfig: TentConfig
+    var listner: ListenerRegistration?
+
     
-    func addImage(image: UIImage){
-      //  imageList.append(TentImage(timeCreated: Date().timeIntervalSince1970, imageURL: ""))
+    init(tentConfig: TentConfig){
+        self.tentConfig = tentConfig
+        tentName = tentConfig.name
+        
+
+        self.listner = db.collection("Tents").document(tentName).collection("Images").addSnapshotListener { querySnapshot, err in
+            guard let snapshot = querySnapshot else {
+                print("Error fetching snapshots: \(err!)")
+                return
+            }
+            
+            snapshot.documentChanges.forEach { diff in
+                print("changes")
+                if (diff.type == .added) {
+                    print("\(diff.document.documentID) => \(diff.document.data())")
+                    if let url = diff.document.data()["URL"] {
+                        self.addImage(image: TentImage(timeCreated: Date().timeIntervalSince1970, imageURL: url as! String ))
+                    }
+                    
+                }
+                if (diff.type == .modified) {
+                    print("Modified city: \(diff.document.data())")
+                }
+                if (diff.type == .removed) {
+                    print("Removed city: \(diff.document.data())")
+                }
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    func updateTent(){
+        print("Updating Tent")
+        guard listner != nil else {
+            return
+        }
+        
+        if(tentConfig.name == tentName){
+            return
+        }
+        
+        //rows = []
+        tentName = tentConfig.name
+        
+        listner!.remove()
+        
+        self.listner = db.collection("Tents").document(tentName).collection("Images").addSnapshotListener { querySnapshot, err in
+            guard let snapshot = querySnapshot else {
+                print("Error fetching snapshots: \(err!)")
+                return
+            }
+            
+            snapshot.documentChanges.forEach { diff in
+                print("changes")
+                if (diff.type == .added) {
+                    print("\(diff.document.documentID) => \(diff.document.data())")
+                    if let url = diff.document.data()["URL"] {
+                        self.addImage(image: TentImage(timeCreated: Date().timeIntervalSince1970, imageURL: url as! String ))
+                    }
+                    
+                }
+                if (diff.type == .modified) {
+                    print("Modified city: \(diff.document.data())")
+                }
+                if (diff.type == .removed) {
+                    print("Removed city: \(diff.document.data())")
+                }
+            }
+            
+        }
+        
+    }
+    
+    func addImage(image: TentImage){
+        imageList.insert(image, at: 0)
     }
 
 }
