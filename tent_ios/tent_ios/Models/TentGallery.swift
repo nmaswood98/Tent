346ec8658c6a25aee4460e6c5f35a3203fdbd71f
nmaswood98 <nmaswood98@gmail.com>
2020-01-26 15:27:24 -0500

    Added downloadChecker for TentImage, added newModel Array to rearrange images

--- tent_ios/tent_ios/Models/TentGallery.swift
+++ tent_ios/tent_ios/Models/TentGallery.swift
@@ -12,7 +12,7 @@ import Kingfisher
 import FirebaseFirestore
 
 class TentGallery: ObservableObject{
-    var imagesToDisplay: [TentImage] = [] // All images
+    var imagesToDisplay: [[TentImage]] = [] // All images
     var imageList: [TentImage] = [] // All images including ones that are not downloaded yet
     
     let db = Firestore.firestore()
@@ -24,38 +24,11 @@ class TentGallery: ObservableObject{
     init(tentConfig: TentConfig){
         self.tentConfig = tentConfig
         tentName = tentConfig.name
-        
 
-        self.listner = db.collection("Tents").document(tentName).collection("Images").addSnapshotListener { querySnapshot, err in
-            guard let snapshot = querySnapshot else {
-                print("Error fetching snapshots: \(err!)")
-                return
-            }
-            
-            snapshot.documentChanges.forEach { diff in
-                print("changes")
-                if (diff.type == .added) {
-                    print("\(diff.document.documentID) => \(diff.document.data())")
-                    if let url = diff.document.data()["URL"] {
-                        self.addImage(image: TentImage(timeCreated: Date().timeIntervalSince1970, imageURL: url as! String ))
-                    }
-                    
-                }
-                if (diff.type == .modified) {
-                    print("Modified city: \(diff.document.data())")
-                }
-                if (diff.type == .removed) {
-                    print("Removed city: \(diff.document.data())")
-                }
-            }
-            
-        }
-        
-        
-        
+        self.updateTent()
     }
     
-    func updateTent(){
+    func removeListnerAndUpdateTent(){
         print("Updating Tent")
         guard listner != nil else {
             return
@@ -65,11 +38,15 @@ class TentGallery: ObservableObject{
             return
         }
         
-        //rows = []
         tentName = tentConfig.name
         
         listner!.remove()
         
+        self.updateTent()
+
+    }
+    
+    func updateTent(){
         self.listner = db.collection("Tents").document(tentName).collection("Images").addSnapshotListener { querySnapshot, err in
             guard let snapshot = querySnapshot else {
                 print("Error fetching snapshots: \(err!)")
@@ -81,7 +58,18 @@ class TentGallery: ObservableObject{
                 if (diff.type == .added) {
                     print("\(diff.document.documentID) => \(diff.document.data())")
                     if let url = diff.document.data()["URL"] {
-                        self.addImage(image: TentImage(timeCreated: Date().timeIntervalSince1970, imageURL: url as! String ))
+                        
+                        let tentImage = TentImage(timeCreated: Date().timeIntervalSince1970, imageURL: url as! String )
+                        tentImage.downloadImage{
+                            result in
+                            if (result){
+                                self.addImage(image: tentImage)
+                            }
+                            else{
+                                print("Couldn't download the image")
+                            }
+                        }
+                        
                     }
                     
                 }
@@ -99,6 +87,27 @@ class TentGallery: ObservableObject{
     
     func addImage(image: TentImage){
         imageList.insert(image, at: 0)
+        self.arrangeImages()
+    }
+    
+    func arrangeImages(){
+        self.imageList.sort{
+            tentImage1, tentImage2 in
+            return tentImage1.timeCreated < tentImage2.timeCreated
+        }
+        
+        var newEntry: [TentImage] = []
+        for image in imageList{
+            if(newEntry.count == 0){
+                newEntry.append(image)
+                imagesToDisplay.append(newEntry)
+            }
+            else{
+                newEntry.append(image)
+                newEntry = []
+            }
+        }
+        
     }
 
 }
